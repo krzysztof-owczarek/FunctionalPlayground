1624e2bfd65a9633170a62f04ad9992278430b45
Krzysztof Owczarek <krzysztofowczarek@icloud.com>
2018-10-20 15:57:48 +0200

    Fixes for structure and leftFold in streams in place of recursive call

--- src/main/java/app/priceModifiers/PriceService.java
+++ src/main/java/app/priceModifiers/PriceService.java
@@ -2,43 +2,62 @@ package app.priceModifiers;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import lombok.AllArgsConstructor;
+import javax.naming.OperationNotSupportedException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
 class PriceService {
-    private OfferContext offerContext;
-    private List<PriceModifier> priceModifiers = new LinkedList<>();
 
-    public PriceService(User currentUser) {
-        offerContext = new OfferContext(currentUser);
-        priceModifiers.add(new PriceModifier(offerContext, x -> offerContext.getCurrentUser().age>18, x -> x*0.5));
-        priceModifiers.add(new PriceModifier(offerContext, x -> offerContext.getCurrentUser().age==30, x -> x-10));
+// RECURSIVE
+//    public double calculate(double price) {
+//        return calculatePriceWithOffers(price, priceModifiers);
+//    }
+//
+//    private double calculatePriceWithOffers(double price, List<PriceModifier> priceModifiers) {
+//        if (priceModifiers.isEmpty()) {
+//            return price;
+//        }
+//
+//        return calculatePriceWithOffers(priceModifiers.get(0).modify(price),
+//                priceModifiers.subList(1, priceModifiers.size()));
+//    }
+
+    public double calculate(double price, SpecialOffer specialOffer) {
+        return specialOffer.getPriceModifiers()
+                .stream()
+                .reduce(price,
+                        (currentPrice, modifier) -> modifier.modify(currentPrice),
+                        ((aDouble, aDouble2) -> {
+                            throw new UnsupportedOperationException();
+                        }));
+    }
+}
+
+class SpecialOffer {
+    private final Map<String, String> offerContext;
+
+    @Getter
+    private List<PriceModifier> priceModifiers;
+
+    SpecialOffer(Map<String, String> offerContext) {
+        this.offerContext = offerContext;
+        this.priceModifiers = new LinkedList<>();
     }
 
-    public double calculate(double price) {
-        return calculatePriceWithOffers(price, priceModifiers);
-    }
-
-    private double calculatePriceWithOffers(double price, List<PriceModifier> priceModifiers) {
-        if (priceModifiers.isEmpty()) {
-            return price;
-        }
-
-        return calculatePriceWithOffers(priceModifiers.get(0).modify(price),
-                priceModifiers.subList(1, priceModifiers.size()));
+    public void createPriceModifier(Predicate<Map<String, String>> condition, Function<Double, Double> modifier) {
+        PriceModifier priceModifier = new PriceModifier(offerContext, condition, modifier);
+        priceModifiers.add(priceModifier);
     }
 }
 
 @RequiredArgsConstructor
 class PriceModifier {
 
-    private final OfferContext offerContext;
-    private final Predicate<OfferContext> condition;
+    private final Map<String, String> offerContext;
+    private final Predicate<Map<String, String>> condition;
     private final Function<Double, Double> modifier;
 
     double modify(double baseValue) {
@@ -49,16 +68,4 @@ class PriceModifier {
         return baseValue;
     }
 }
-
-@Getter
-@RequiredArgsConstructor
-class OfferContext {
-    private final User currentUser;
-}
-
-@AllArgsConstructor
-class User {
-    int age;
-}
-
 
--- src/test/java/app/priceModifiers/PriceSpec.groovy
+++ src/test/java/app/priceModifiers/PriceSpec.groovy
@@ -1,29 +1,58 @@
 package app.priceModifiers
 
+import spock.lang.Shared
 import spock.lang.Specification
 
 class PriceSpec extends Specification {
-    def "test modifiers"() {
+
+    @Shared priceService = new PriceService()
+
+    def "test modifiers for user age"() {
         given: "PriceService and logged user, +18 years old"
-        def currentUser = new User(19)
-        def priceService = new PriceService(currentUser)
+        def offerContext = new HashMap()
+        offerContext.put("age", "19")
+
+        def specialOffer = new SpecialOffer(offerContext)
+        specialOffer.createPriceModifier({ x -> Integer.valueOf(offerContext.get("age"))>18 }, { x -> x*0.5 })
 
         when: "price 100 with offers"
-        def priceWithOffers = priceService.calculate(100)
+        def priceWithOffers = priceService.calculate(100, specialOffer)
 
         then: "expect price 50% lower"
         priceWithOffers == 50
     }
 
-    def "test modifiers2"() {
+    def "test modifiers for 2 combined modifications"() {
         given: "PriceService and logged user, exactly 30 years old, fires two modifiers"
-        def currentUser = new User(30)
-        def priceService = new PriceService(currentUser)
+        def offerContext = new HashMap()
+        offerContext.put("age", "30")
+
+        def specialOffer = new SpecialOffer(offerContext)
+        specialOffer.createPriceModifier({ x -> Integer.valueOf(offerContext.get("age"))>18 }, { x -> x*0.5 })
+        specialOffer.createPriceModifier({ x -> Integer.valueOf(offerContext.get("age"))==30 }, { x -> x-10 })
 
         when: "price 100 with offers"
-        def priceWithOffers = priceService.calculate(100)
+        def priceWithOffers = priceService.calculate(100, specialOffer)
 
         then: "expect price 50% and -10 lower"
         priceWithOffers == 40
+    }
+
+    def "test modifiers for 2 combined modifications and another 5\$ for every Bob!"() {
+        given: "PriceService and logged user, exactly 30 years old, fires two modifiers"
+        def offerContext = new HashMap()
+        offerContext.put("age", "30")
+        offerContext.put("name", "Bob")
+
+        def specialOffer = new SpecialOffer(offerContext)
+        specialOffer.createPriceModifier({ x -> Integer.valueOf(offerContext.get("age"))>18 }, { x -> x*0.5 })
+        specialOffer.createPriceModifier({ x -> Integer.valueOf(offerContext.get("age"))==30 }, { x -> x-10 })
+        specialOffer.createPriceModifier({ x -> "Bob".equals(offerContext.get("name")) }, { x -> x-5 })
+
+        when: "price 100 with offers"
+        def priceWithOffers = priceService.calculate(100, specialOffer)
+
+        then: "expect price 50% and -10 lower and -5 lower"
+        priceWithOffers == 35
     }
 }
